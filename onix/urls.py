b6214b566672343a57cd367bb204cd1a757292aa
Onixpetroleum <emperorceaser120@gmail.com>
2026-08-03 13:37:53 +0100

    Add password reset URLs to urlpatterns

--- onix/urls.py
+++ onix/urls.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 from django.contrib.auth import views as auth_views
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Password reset
+
+    # Password Reset URLs
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
